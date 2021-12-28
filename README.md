07f7a0a4239ee9bd0ae2e91e359864a14c39ca3a
João Diogo Martins Romão <up201806779@fe.up.pt>
2021-12-28 12:30:29 +0000

    Added tutorial

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # SDLE Project
 
-SDLE Project for group T&lt;n&gt;G&lt;p&gt;&lt;q&gt;.
+SDLE Project for group T4G15.
 
 Group members:
 
-1. &lt;first name&gt; &lt;family name&gt; (&lt;email address&gt;)
-2. &lt;first name&gt; &lt;family name&gt; (&lt;email address&gt;)
-3. &lt;first name&gt; &lt;family name&gt; (&lt;email address&gt;)
-4. &lt;first name&gt; &lt;family name&gt; (&lt;email address&gt;)
+1. João Pires (up201806079@edu.fe.up.pt)
+2. João Romão (up201806779@edu.fe.up.pt)
+3. Rafael Cristino (up201806680@edu.fe.up.pt)
+4. Xavier Pisco (up201806134@edu.fe.up.pt)
--- src/peer/src/peer.ts
+++ src/peer/src/peer.ts
@@ -1,7 +1,61 @@
 #!/usr/bin/env node
 
-export const hello = () => {
-    console.log('Hello World')
+import libp2p from 'libp2p'
+import tcp from 'libp2p-tcp'
+import { NOISE } from 'libp2p-noise'
+// @ts-ignore
+import mplex from 'libp2p-mplex'
+import process from 'process'
+import { multiaddr } from 'multiaddr'
+
+const startLibp2p = async (node: libp2p) => {
+    await node.start()
+    console.log('libp2p has started')
 }
 
-hello()
+const stopLibp2p = async (node: libp2p) => {
+    await node.stop()
+    console.log('libp2p has stopped')
+    process.exit(0)
+}
+
+const logAdresses = (node: libp2p) => {
+    console.log('listening on addresses:')
+    node.multiaddrs.forEach(addr => {
+        console.log(`${addr.toString()}/p2p/${node.peerId.toB58String()}`)
+    })
+}
+
+const ping = async (node: libp2p) => {
+    if (process.argv.length >= 3) {
+        const ma = multiaddr(process.argv[2])
+        console.log(`pinging remote peer at ${process.argv[2]}`)
+        const latency = await node.ping(ma)
+        console.log(`pinged ${process.argv[2]} in ${latency}ms`)
+    } else {
+        console.log('no remote peer address given, skipping ping')
+    }
+}
+
+const main = async () => {
+    const node: libp2p = await libp2p.create({
+        addresses: {
+            // add a listen address (localhost) to accept TCP connections on a random port
+            listen: ['/ip4/127.0.0.1/tcp/0']
+        },
+        modules: {
+            transport: [tcp],
+            connEncryption: [NOISE],
+            streamMuxer: [mplex]
+        }
+    })
+
+    await startLibp2p(node)
+    logAdresses(node)
+    ping(node)
+
+    process.on('SIGTERM', () => stopLibp2p(node))
+    process.on('SIGINT', () => stopLibp2p(node))
+}
+
+main()
